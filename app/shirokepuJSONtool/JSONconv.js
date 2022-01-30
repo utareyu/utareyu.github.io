aa5e6ac4ff564f28af75171822b2def24775907f
utareyu <uta5315@gmail.com>
2022-01-30 19:12:15 +0900

    -

--- app/shirokepuJSONtool/JSONconv.js
+++ app/shirokepuJSONtool/JSONconv.js
@@ -1,45 +1,49 @@
 function runConv(date,data,desc=null){
-	// const shiro= fetch('https://raw.githubusercontent.com/McbeEringi/shirokepu/main/main.json'); 
-	// console.log(shiro);
-
-	// no = Math.max.apply(null,shiro.map(function(o){return o.speed;}))-1
-
-	let memberE = document.getElementsByName('member');
-	let tagE = document.getElementsByName('tag');
-	let member = 0;
-	let tag = 0;
-	let newWord ="";
-
-	for (let i = 0; i < 4; i++){
-			if (memberE.item(i).checked){
-					member = memberE.item(i).value;
-			}
-	}
-
-	for (let i = 0; i < 4; i++){
-			if (tagE.item(i).checked){
-					tag = tagE.item(i).value;
-			}
-	}
-
-	date=date.replace(/-/g,"");
-	date=date.slice(2);
-	desc=desc.replace(/\n/g," ");
-	//{"no":nnn, "member":[0~3], "date":yymmdd, "tag":[0~3], "data":"XXX", "desc":"XXX"}   , "desc":${desc}}
-	// newWord=`{"no":${no}, "member":${member}, "date":${date}, "tag":[${tag}], "data":"${data}"`;
-	newWord=`{"no":nnn, "member":${member}, "date":${date}, "tag":[${tag}], "data":"${data}"`;
-	if(desc){
-		newWord+=`, "desc":"${desc}"}`
+	if(date=="" || data==""){
+		return;
 	} else {
-		newWord+=`}`
+		// const shiro= fetch('https://raw.githubusercontent.com/McbeEringi/shirokepu/main/main.json'); 
+		// console.log(shiro);
+
+		// no = Math.max.apply(null,shiro.map(function(o){return o.speed;}))-1
+
+		let memberE = document.getElementsByName('member');
+		let tagE = document.getElementsByName('tag');
+		let member = 0;
+		let tag = 0;
+		let newWord ="";
+
+		for (let i = 0; i < 4; i++){
+				if (memberE.item(i).checked){
+						member = memberE.item(i).value;
+				}
+		}
+
+		for (let i = 0; i < 4; i++){
+				if (tagE.item(i).checked){
+						tag = tagE.item(i).value;
+				}
+		}
+
+		date=date.replace(/-/g,"");
+		date=date.slice(2);
+		desc=desc.replace(/\n/g," ");
+		//{"no":nnn, "member":[0~3], "date":yymmdd, "tag":[0~3], "data":"XXX", "desc":"XXX"}   , "desc":${desc}}
+		// newWord=`{"no":${no}, "member":${member}, "date":${date}, "tag":[${tag}], "data":"${data}"`;
+		newWord=`{"no":nnn, "member":${member}, "date":${date}, "tag":[${tag}], "data":"${data}"`;
+		if(desc){
+			newWord+=`, "desc":"${desc}"}`
+		} else {
+			newWord+=`}`
+		}
+
+		// console.log(no,",", member,",", date,",", tag,",", data,",", desc);
+		console.log(member,",", date,",", tag,",", data,",", desc);
+		console.log(newWord);
+
+			document.getElementById("new").value = newWord;
+			document.getElementById('copybtn').value="コピー";
 	}
-
-// console.log(no,",", member,",", date,",", tag,",", data,",", desc);
-// console.log(member,",", date,",", tag,",", data,",", desc);
-// console.log(newWord);
-
-	document.getElementById("new").value = newWord;
-	document.getElementById('copybtn').value="コピー";
 }
 
 function copynew(){
@@ -50,5 +54,4 @@ function copynew(){
 			.catch(err => {
 				document.getElementById('copybtn').value="なんでかコピーできないよ";
 	})
-
 }
