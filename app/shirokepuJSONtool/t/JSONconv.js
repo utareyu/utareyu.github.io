0280099921c1a3ccd680abe2f72290d0c2dbc076
utareyu <uta5315@gmail.com>
2022-04-26 18:51:18 +0900

    -

--- app/shirokepuJSONtool/t/JSONconv.js
+++ app/shirokepuJSONtool/t/JSONconv.js
@@ -1,4 +1,4 @@
-function runConv(date,data,desc=null){
+function runConv(date,data,desc=null,img=null){
 	let memberE = document.getElementsByName('member');
 	let tagE = document.getElementsByName('tag');
 	let member = 0;
@@ -17,18 +17,28 @@ function runConv(date,data,desc=null){
 					tag += tagE.item(i).value;
 			}
 	}
+
 	if(date=="" || data=="" || tag==""){
 		convbtn.value="入力が必要な項目があるよ"
 		return;
 	} else {
 		date=date.replace(/-/g,"").slice(2);
-		desc=desc.replace(/\n/g," ");
+		desc=desc.replace(/\n/g,"<br>");
 		newWord=`\n\t\t{"no":nnn, "member":${member}, "date":${date}, "tag":[${tag}], "data":"${data}"`;
 		if(desc){
 			newWord+=`, "desc":"${desc}"},`
 		} else {
 			newWord+=`},`
 		}
+		if(img){
+			
+		}
+		/*
+		有効なデータが入っているときに
+			imgのファイル名取得
+			
+		*/
+		//<img src=\"img/photos/Screenshot_20210913-235626.png\" width=200>
 		
 		console.log(newWord);
 
@@ -51,4 +61,12 @@ function copynew(){
 	}else{
 		copybtn.value="結果が空欄だよ";
 	}
+}
+
+function previewImage(obj){
+	var fileReader = new FileReader();
+	fileReader.onload = (function() {
+		document.getElementById('preview').src = fileReader.result;
+	});
+	fileReader.readAsDataURL(obj.files[0]);
 }
